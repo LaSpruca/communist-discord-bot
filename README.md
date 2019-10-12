a6b5e65dc54d68a32944dbe71cc55c13e91e5e04
LaSpruca <nathanahre32@gmail.com>
2019-10-12 22:16:14 +1300

    Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#THE MOST BLAYTIFUL DISCORD BOT IN EXISTENCE
+# THE MOST BLAYTIFUL DISCORD BOT IN EXISTENCE
 This bot makes discord blaytiful by adding communism
---
+++ src/main/java/me/nathan/Auth.java
@@ -0,0 +1,6 @@
+package me.nathan;
+
+public class Auth {
+    public static String distToken = "NTk2MTAwMDQ0ODIyOTM3NjMw.XaF5PQ.Zn0Pp5vKNk9CTlbqK-h2bCHKC-o";
+    public static String devToken = "";
+}
