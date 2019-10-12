56781c53b6d78fb7edcb57962ecea600e802ef98
LaSpruca <nathanahre32@gmail.com>
2019-10-12 22:33:17 +1300

    Minor bug fix

--- src/main/java/me/nathan/Auth.java
+++
@@ -1,6 +0,0 @@
-package me.nathan;
-
-public class Auth {
-    public static String distToken = "NTk2MTAwMDQ0ODIyOTM3NjMw.XaF5PQ.Zn0Pp5vKNk9CTlbqK-h2bCHKC-o";
-    public static String devToken = "";
-}
